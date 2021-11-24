204b58d4cbf13a7b653a757d698306352c65df24
Daniel Pacuraru <pacurarudaniel@gmail.com>
2021-11-24 23:35:06 +0200

    removed puppeteer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PuppeteerModule } from 'nest-puppeteer';
 
 import { KatalogModule } from './katalog/katalog.module';
 
 @Module({
   imports: [
-    PuppeteerModule.forRoot(),
     KatalogModule
   ]
 })
