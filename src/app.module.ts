7271c80a5861a12828a4afa1fbf3b25f485082c3
Daniel Pacuraru <pacurarudaniel@gmail.com>
2021-11-25 17:22:59 +0200

    puppeteer tweak

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PuppeteerModule } from 'nest-puppeteer';
+import { LaunchOptions } from 'puppeteer';
 
 import { KatalogModule } from './katalog/katalog.module';
 
 @Module({
   imports: [
-    PuppeteerModule.forRoot(),
+    PuppeteerModule.forRoot(<LaunchOptions>{
+      headless: true,
+      args: ['--no-sandbox', '--disable-dev-shm-usage']
+    }),
     KatalogModule
   ]
 })
