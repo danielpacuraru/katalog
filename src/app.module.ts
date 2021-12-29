cbcf618b436497ea115d5a4a488ceb453bb5c2f0
Daniel Pacuraru <pacurarudaniel@gmail.com>
2021-12-29 12:12:13 +0200

    progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 import { ConfigsModule } from './config/config.module';
 import { AuthModule } from './auth/auth.module';
@@ -7,6 +9,12 @@ import { KatalogModule } from './katalog/katalog.module';
 @Module({
   imports: [
     ConfigsModule,
+    MongooseModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        uri: config.get('MONGO_URL')
+      }),
+      inject: [ConfigService]
+    }),
     AuthModule,
     KatalogModule
   ]
